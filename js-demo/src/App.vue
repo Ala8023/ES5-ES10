dadda0a45382b62f4080b3ac795fd0a77ec35fa9
wuna <wuna@19lou.com>
2020-04-14 22:10:29 +0800

    init

---
+++ js-demo/src/App.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: wuna
+ * @Date: 2020-04-14 21:55:41
+ * @LastEditors: wuna
+ * @LastEditTime: 2020-04-14 22:02:31
+ -->
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
